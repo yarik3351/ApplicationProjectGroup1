11fee3d04448113df69d4fa635f11d2351aca77b
stepp4k <94464312+stepp4k@users.noreply.github.com>
2022-03-01 14:45:40 -0500

    Add Template for posts

--- find-your-freshman/src/pages/HomePage.tsx
+++ find-your-freshman/src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DateDisplay from '../components/DateDisplay'
 import { v4 as uuidv4 } from 'uuid'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+
+
+import { Container, Card, Button } from 'react-bootstrap'
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate()
@@ -14,11 +18,35 @@ const HomePage: React.FC = () => {
         }
     }, [])
 
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = async (params: any) => {
+        const db = getFirestore()
+        const querySnapshot = await getDocs(collection(db, "posts"));
+        querySnapshot.forEach((doc) => {
+            //setPosts([...posts, doc.data.postText]);
+            console.log(doc.id, " => ", doc.data());
+        });
+
+    }
+
+
+
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <h1 key={uuidv4()} style={{ fontSize: '4em' }}>
                 Hello world!
             </h1>
+            <Card key={uuidv4()} style={{ width: '18rem' }}>
+                <Card.Img key={uuidv4()} variant="top" src="" />
+                <Card.Body key={uuidv4()}>
+                    <Card.Title key={uuidv4()}>by John Doe</Card.Title>
+                    <Card.Text key={uuidv4()}>
+                        This is post content
+                    </Card.Text>
+                    
+                </Card.Body>
+            </Card>
             <DateDisplay key={uuidv4()} />
         </div>
     )
